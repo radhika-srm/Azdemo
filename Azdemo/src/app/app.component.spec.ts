3332221f5b9e497c96742ce818e85b0ede532c1e
radhika-srk <radhika.srk33@gmail.com>
2024-04-13 15:55:52 +0530

    First commit

--- Azdemo/src/app/app.component.spec.ts
+++ Azdemo/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angulardemo' title`, () => {
+  it(`should have the 'Azdemo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angulardemo');
+    expect(app.title).toEqual('Azdemo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angulardemo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Azdemo');
   });
 });
